6f3b2ccc0eeb7304a3f206a5134d847bf05d075f
Kneesham <vlog.car.guy@gmail.com>
2020-04-18 01:02:39 -0500

    adding files

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const v1Router = require("./routes/v1/v1Routes");
 const v2Router = require("./routes/v2/v2Routes");
 const logger = require("./lib/middleware/logger");
 
-
-
 const app = express();
+const port = 3000;
 
 app.use(logger);
 app.use("/v1", v1Router);
 app.use("/v2", v2Router);
-
-const port = 3000;
 app.listen(port);
-
 
 console.log("listening on port: " + port);
--- routes/v2/v2Routes.js
+++ routes/v2/v2Routes.js
@@ -1,37 +1,165 @@
 const express = require("express");
-const axios = require("axios");
 const mongoose = require("mongoose");
-const mongoUrl = "mongodb://127.0.0.1:3000/xfiles";
+const bodyParser = require("../../lib/middleware/bodyParser");
+const mongoUrl = "mongodb://127.0.0.1:27017/project2";
+const UserPost = require("./UserPost");
+const User = require("./user");
 
+const makePost = async (req, res) => {
+    // creates a new post for a specific user
 
-const getAllPosts = (req, res) => {
+    console.log(req.body);
+    try {
+        mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true} );
+        const userPost = new UserPost(req.body);
+        const results = await userPost.save();
+        res.send(results);
+        mongoose.disconnect(mongoUrl);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500);
+        res.send(error);
+    }
+}
+
+const editPost = async (req, res) => {
+    // updates a specific post
+
+    const postId = req.params.postId;
+    
+    try {
+        mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true} );
+        const post_Id = await UserPost.find({id: postId});
+        // used to get the long _id
+        console.log(post_Id[0]._id);
+
+        const postsToEdit = await UserPost.findById(post_Id[0]._id).exec();
+        postsToEdit.set(req.body);
+        const result = await postsToEdit.save();
+        res.send( result );
+        mongoose.disconnect(mongoUrl);
+    }
+    catch (error) {
+        console.error("error: ", error);
+        res.status(500);
+        res.send(error);
+    } 
+}
+
+const deletePost = async (req, res) => {
+    // removes a particular post
+    const postId = req.params.postId;
+    try {
+        mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true} );
+        const results = await UserPost.deleteOne({ _id: postId }).exec();
+        res.send(results);
+        mongoose.disconnect(mongoUrl);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500);
+        res.send(error);
+    }
+
+}
+
+const getAllPosts = async (req, res) => {
     // Returns all posts for all users.
-    res.send("getting stuff")
+
+    try {
+        mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true} );
+        const results = await UserPost.find().exec();
+        res.send(results);
+        mongoose.disconnect(mongoUrl);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500);
+        res.send(error);
+    }
 };
 
-const getUsersPosts = (req, res) => {
+const getUsersPosts = async (req, res) => {
     // Returns all posts for a specific user, by using their username.
     const userName = req.params.userName;
 
+    try {
+        mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true} );
+        const user = await User.find({username: userName});
+        const userId = user[0].id;
+        const userPosts = await UserPost.find({userId: userId})
+        res.send(userPosts);
+        mongoose.disconnect(mongoUrl);
+    }
+    catch (error) {
+        console.error("error: ", error);
+        res.status(500);
+        res.send(error);
+    } 
+
 };
 
-const getPostsById = (req, res) => {
+const getPostsById = async (req, res) => {
     // returns a specific post by its ID number
     const postId = req.params.postId;
+    
+    try {
+        mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true} );
+        const post_Id = await UserPost.find({id: postId});
+        // used to get the long _id
 
+        const postsToReturn = await UserPost.findById(post_Id[0]._id).exec();
+        res.send( postsToReturn );
+        mongoose.disconnect(mongoUrl);
+    }
+    catch (error) {
+        console.error("error: ", error);
+        res.status(500);
+        res.send(error);
+    } 
 };
 
-const getUsersName = (req, res) => {
+const getUsersName = async (req, res) => {
     //Returns a specific user's name.
     const userName = req.params.userName;
-    res.send("getting the user's name by username\n " + userName + ":name");
+
+    try {
+        mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true} );
+        const result = await User.find({username: userName});
+
+        res.send( "That user's name is: " + result[0].name );
+        mongoose.disconnect(mongoUrl);
+    }
+    catch (error) {
+        console.error("error: ", error);
+        res.status(500);
+        res.send(error);
+    } 
 };
 
-const v1Router = express.Router();
-v1Router.get("/allPosts", getAllPosts);
-v1Router.get("/allPosts/:userName", getUsersPosts);
-v1Router.get("/posts/:postId", getPostsById);
-v1Router.get("/profile/:userName", getUsersName);
+const v2Router = express.Router();
 
+v2Router
+.route("/allPosts")
+.get(getAllPosts);
 
-module.exports = v1Router;
+v2Router
+.route("/allPosts/:userName")
+.get(getUsersPosts);
+
+v2Router
+.route("/profile/:userName")
+.get(getUsersName);
+
+v2Router
+.route("/posts")
+.post(bodyParser.json(), makePost);
+
+v2Router
+.route("/posts/:postId")
+.get(getPostsById)
+.patch(bodyParser.json(), editPost)
+.delete(deletePost);
+
+module.exports = v2Router;
