3173071cc0062ca84e0620f39d1aa9bb5abc003e
Kneesham <vlog.car.guy@gmail.com>
2020-04-17 19:08:01 -0500

    Finished product of route V1

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const v1Router = require("./routes/v1Routes");
+const v1Router = require("./routes/v1/v1Routes");
+const v2Router = require("./routes/v2/v2Routes");
+const logger = require("./lib/middleware/logger");
+
+
 
 const app = express();
 
-app.use(v1Router);
-
+app.use(logger);
+app.use("/v1", v1Router);
+app.use("/v2", v2Router);
 
 const port = 3000;
 app.listen(port);
